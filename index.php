3bd51746850f21fdc81910954307e3eaec59caed
Otto Hooper <votton@nyanpasu.tv>
2017-09-13 18:16:46 +0100

    Some changes.
    Some changes.

--- index.php
+++ index.php
@@ -16,22 +16,22 @@
 </head>
 
 <body>
-  <div class=container>
+
+<div class=container>
     <div class=jumbotron>
-      <h1>Ohay&#x14D;!</h1>
-      <p class=lead>100MiBs the Limit</p><noscript><p class="alert alert-error"><strong>Please Enable JavaScript</strong> Here is a non javascript version of the website as a other choice: <a href="https://pomfe.co/nojs.html">https://pomfe.co/nojs.html</a></p></noscript>
-      <p id=no-file-api class="alert alert-error"><strong>This site requires some cool Web 2.0 stuff</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Botnet</a> and come back &lt;3</p><a href="javascript:;" id=upload-btn class=btn>Select <span>or drop</span> file(s)</a>
-      <input type=file id=upload-input name="files[]" multiple data-max-size=100MiB>
-      <ul id=upload-filelist></ul>
+        <h1>Ohay&#x14D;!</h1>
+        <p class=lead>100MiBs the Limit</p><noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> Here is a non javascript version of the website: <a href="https://pomfe.co/nojs.html">https://pomfe.co/nojs.html</a></p></noscript>
+        <p id=no-file-api class="alert alert-error"><strong>This site requires some cool Web 2.0 stuff</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Botnet</a> and come back &lt;3</p><a href="javascript:;" id=upload-btn class=btn>Select <span>or drop</span> file(s)</a>
+        <input type=file id=upload-input name="files[]" multiple data-max-size=100MiB>
+        <ul id=upload-filelist></ul>
 
-    </div>
-    <script async src=pomf.min.js></script>
-  </div>
+        <div class="footer">
+            <?php include('global/footer.html'); ?>
+        </div>
 
-  <div class="footer">
-    <?php include('global/footer.html'); ?>
-  </div>
+        <script async src=pomf.min.js></script>
 
+  <div class="background-img"></div>
 </body>
 
 </html>
