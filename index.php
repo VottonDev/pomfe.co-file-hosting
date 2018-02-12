088b280411ef206e5eeac6dae129cabf68ccdd3f
Otto Hooper <votton@nyanpasu.tv>
2018-02-12 09:37:59 +0000

    Fixing a spelling error
    Fixing a spelling error

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 <div class=container>
     <div class=jumbotron>
         <h1>Ohay&#x14D;!</h1>
-        <p class=lead>100MiBs the Limit</p><noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> Here is a non javascript version of the website: <a href="https://pomfe.co/nojs.html">https://pomfe.co/nojs.html</a></p></noscript>
+        <p class=lead>100MiBs is the limit</p><noscript><p class="alert alert-error"><strong>Enable JavaScript</strong> Here is a non javascript version of the website: <a href="https://pomfe.co/nojs.html">https://pomfe.co/nojs.html</a></p></noscript>
         <p id=no-file-api class="alert alert-error"><strong>This site requires some cool Web 2.0 stuff</strong> Install the latest<a href="https://firefox.com/">Firefox</a> or <a href="https://chrome.google.com/">Botnet</a> and come back &lt;3</p><a href="javascript:;" id=upload-btn class=btn>Select <span>or drop</span> file(s)</a>
         <input type=file id=upload-input name="files[]" multiple data-max-size=100MiB>
         <ul id=upload-filelist></ul>
@@ -31,10 +31,9 @@
         </div>
 
         <p class="alert alert-info">
-        <strong>Pomfe.co is a free service and we intend to keep it this way, but we need your help</strong> &mdash; we here at Pomfe want to keep any and all donations transparent, as a proof of concept, we need exactly 10$ each month to pay for our bills and keep this service free forever. As a donator, you'll receive 100% transparency on the money you donate, whether it's upgrades, monthly bills or other expenses. You can help us by donating via Patreon or Bitcoin, thanks a lot!
+        <strong>Pomfe.co is a free service, but our hosting bill isn't</strong> &mdash; we need exactly 35$ each month to pay for our bills and keep this service free forever. As a donator, you'll receive 100% transparency on the money you donate, whether it's upgrades, monthly bills or other expenses. Thanks a lot!
 <span class="donate-btns"><a class="donate-btn donate-bitcoin" href="bitcoin:1Jx1PSYdqwrhtYGhUvVvYZRfKScHQ9VLRH?label=tpb.lc&amp;message=Hosting%20Bills" target="_BLANK"><span class="icon icon-bitcoin"></span> Bitcoin</a><a class="donate-btn donate-patreon" href="https://www.patreon.com/Votton" target="_BLANK"><span class="icon icon-patreon"></span> Patreon</a></div>
 
-  <div class="background-img"></div>
 </body>
 
 </html>
